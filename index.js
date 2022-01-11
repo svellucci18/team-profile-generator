b97c247c05b61ef906f9687006e53d5ec16d2e7c
svell <svellucci18@gmail.com>
2022-01-10 18:39:48 -0800

    psuedocode from class demo

---
+++ index.js
@@ -0,0 +1,42 @@
+// Declare the object that will store all the info in the global scope.
+const employees = [];
+
+// Actions needed
+
+// Ask for manager info
+
+    // Prompt user for the data
+
+        // THEN create and store an object for the Manager
+
+        // THEN whatNext()
+
+// Ask for engineer info
+
+    // Prompt user for the data
+
+        // THEN create and store an object for the Engineer
+
+        // THEN whatNext()
+
+
+// Ask for intern info
+
+    // Prompt user for the data
+
+        // THEN create and store an object for the Intern
+
+        // THEN whatNext()
+
+
+// Ask what they would like to do next whatNext()
+
+    // Add Engineer, Add Intern or All done
+
+        // IF `Add Engineer` -> `Ask for engineer info`
+
+        // IF `Add Intern` -> `Ask for intern info`
+
+        // IF `All done` -> `build an html page`
+
+// Use all of the information to build an html page

23644cf4e7461c8f76c3a6fc8795cfc5f066aaf1
svell <svellucci18@gmail.com>
2022-01-11 21:24:54 -0800

    attempted to write a test, cleaned up the template html, wrote out classes, wrote a functioning prompt for the first few questions

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,1 +1,19 @@
 const Employee = require('../lib/employee');
+
+describe("Employee", () => {
+    // Test for all use cases when initializing a new Employee object
+    it("should create an object with a name id and email properties", () => {
+
+        // Arrange
+        const employee = new Employee("Samantha", 1, "svellucci18@gmail.com");
+    
+        // Act
+        
+
+        // Assert
+        expect(employee.name).toEqual("Samantha");
+        expect(employee.id).toEqual(1);
+        expect(employee.email).toEqual("svellucci18@gmail.com");
+    })
+
+})
--- index.js
+++ index.js
@@ -1,15 +1,57 @@
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// const generateHTML = require('./utils/generateHTML');
+// const path = require('path');
+// const prompts = new Rx.Subject(); // Review Reactive interface on inquirer docs
+
 // Declare the object that will store all the info in the global scope.
 const employees = [];
 
-// Actions needed
+// Create an array of questions for user input SHOULD THIS BE employees and use template literals to grab employee type?
+const managerQuestions = [
+    {
+        type: 'input',
+        message: "What is the team manager's name?",
+        name: 'name',
+    },
+    {
+        type: 'number',
+        message: "What is the team manager's id?",
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: "What is the team manager's email?",
+        name: 'email',
+    },
+    {
+        type: 'number',
+        message: "What is the team manager's office number?",
+        name: 'officeNumber',
+    },
+];
 
 // Ask for manager info
-
-    // Prompt user for the data
+function init() { // WOULD THIS BE BETTER AS AN ASYNC AWAIT FUNCTION
+    inquirer
+        // Prompt user for the data
+        .prompt(
+            // `Welcome to the team generator!
+            // Use 'npm run reset' to reset the dist/ folder`
+            managerQuestions)
 
         // THEN create and store an object for the Manager
+        .then((response) => {
+            console.log(response);
+        })
 
         // THEN whatNext()
+
+        // THEN writeFile()
+}
+
+
 
 // Ask for engineer info
 
@@ -40,3 +82,4 @@ const employees = [];
         // IF `All done` -> `build an html page`
 
 // Use all of the information to build an html page
+init();
